aff08197a75364267f029f068ee3dceeeb65097b
Clark1306 <45311467+Clark1306@users.noreply.github.com>
2018-11-30 19:07:47 +0800

    Update Takashima.py

--- Takashima.py
+++ Takashima.py
@@ -8,7 +8,33 @@ bot = commands.Bot(command_prefix="c!")
 
 @bot.event
 async def on_ready():
+    await client.change_presence(game=Game(name="Nya~!"))
     print("I'm ready!")
+
+@client.command(name='8ball',
+                description="Answers a yes/no question.",
+                brief="Answers from the beyond.",
+                aliases=['eight_ball', 'eightball', '8-ball'],
+                pass_context=True)
+async def yn(context):
+    possible_responses = [
+        'Ehhh, Maybe?',
+        'Yes.',
+        'No.',
+        'No! Why would you ask that?!',
+        'No u',
+        'Probably, A yes.',
+        'Indeed.',
+        'True.',
+        'Just leave me alone.',
+        'Eck! No! Why would you ask that?',
+        'Heck no! get away from me!',
+        'Uh huh, Maybe..'
+        'I call thats a definitely a yes.',
+        'Probably?',
+        'Can we just talk about something else instead of that?',
+    ]
+    await client.say(random.choice(possible_responses) + ", " + context.message.author.mention)
 
 @bot.command(pass_context = True)
 async def say(ctx, *args):

72e16e66bc030ef4a83d2ac064ffae59d22870b4
Clark1306 <45311467+Clark1306@users.noreply.github.com>
2018-11-30 17:10:15 +0800

    Create Takashima.py

---
+++ Takashima.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncio
+
+bot = commands.Bot(command_prefix="c!")
+
+@bot.event
+async def on_ready():
+    print("I'm ready!")
+
+@bot.command(pass_context = True)
+async def say(ctx, *args):
+    mesg = ' '.join(args)
+    await bot.delete_message(ctx.message)
+    return await bot.say(mesg)
+
+bot.run(os.environ['BOT_TOKEN'])

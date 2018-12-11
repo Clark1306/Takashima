250a11056bfff7970b21b7097cbf98b3f52b6a55
Clark1306 <45311467+Clark1306@users.noreply.github.com>
2018-12-11 18:15:30 +0800

    Update Takashima.py

--- Takashima.py
+++ Takashima.py
@@ -57,9 +57,25 @@ async def help(ctx):
     embed.add_field(name='c!8ball', value='Questions your answer!', inline=False)
     embed.add_field(name='Moderation Command', value='.', inline=False)
     embed.add_field(name='c!kick', value='Boots a person from your server or whatever.', inline=False)
+    embed.add_field(name='Other Commands', value='.', inline=False)
+    embed.add_field(name='c!about', value='Knowing about me! Well, kinda bad explain and stuff, i will improve it later.', inline=False)
     embed.set_footer(text="There would be still more commands to be added, so yeah.")
     
     await bot.send_message(author, embed=embed)
+
+@bot.command(pass_context=True)
+async def about(ctx):
+    embed = discord.Embed(
+        color = discord.Color.blue()
+    )
+
+embed.set_author(name="About me")
+embed.add_field(name="Hello! My name is Takashima, i'm just a unoffical neko bot! i have no Tons of commands added yet so, it'll be added soon.", value='(kay)', inline=False)
+embed.add_field(name='I was created by Clark#8056', value='(Just to be honest why do i even make this)', inline=False)
+embed.add_field(name="I was wondering of my creator would make his server and make me offical, until he is good at programming.", value=(meh), inline=False)
+embed.add_field(name='Thats it! Have a good day! nya~!', value='(shut up.)', inline=False)
+
+await bot.message_send(embed=embed)
 
 @bot.command(name='8ball',
                 description="Answers a yes/no question.",

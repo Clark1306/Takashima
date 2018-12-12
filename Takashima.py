6888dc5ed5f23315d0e052388a9b41192cd22b80
Clark1306 <45311467+Clark1306@users.noreply.github.com>
2018-12-12 11:20:44 +0800

    Update Takashima.py

--- Takashima.py
+++ Takashima.py
@@ -14,11 +14,6 @@ async def on_ready():
     await bot.change_presence(game=discord.Game(name="Nya~!"))
     print("I'm ready!")
 
-@bot.event
-async def on_member_join(member):
-    role = discord.utils.get(member.server.roles, name='Vice Admiral')
-    await bot.add_roles(member, role)
-
 @bot.command(pass_context=True,description="Kicks the given member. Please ensure both the bot and the command invoker have the permission 'Kick Members' before running this command.")
 async def kick(ctx, target:discord.Member):
     """Boot someone outta the server. See 'c!help' for more."""

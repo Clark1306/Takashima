f8b213a91b834c545602602ee533ed18c4c7a197
Clark1306 <45311467+Clark1306@users.noreply.github.com>
2018-12-24 17:53:44 +0800

    Update Takashima.py

--- Takashima.py
+++ Takashima.py
@@ -104,7 +104,7 @@ async def yn(context):
     await bot.say(random.choice(possible_responses) + " " + context.message.author.mention)
 
 @bot.command(pass_context=True)
-async def delete_channel(ctx, channel: discord.Channel):
+async def delete_channel(server, channel):
     server = ctx.message.server
     await bot.delete_channel(channel.id)
 

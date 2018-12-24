7ea4d83c870e55736577908a6f115ec025847d8d
Clark1306 <45311467+Clark1306@users.noreply.github.com>
2018-12-24 20:52:15 +0800

    Update Takashima.py

--- Takashima.py
+++ Takashima.py
@@ -65,16 +65,12 @@ async def help(ctx):
 
 @bot.command(pass_context=True)
 async def about(ctx):
-    embed = discord.Embed(
-        title="Hello! I'm Takashima-chan, an unofficial neko bot created by Clark#8056 and  AJ#2121! I have moderation and fun commands, some are still being added tho! I'm still being worked on by the developers, there are some errors here and there, but they're trying their best to finish me and add updates!",
-        color = discord.Color.blue()
-    )
-
-    embed.set_author(name="===================================================================")
-    embed.add_field(name="===================================================================", value="That's all for now, have a good day! nyah~", inline=True)
-    embed.set_footer(text="===============================================================================")
-
-    await bot.say(embed=embed)
+    embed=discord.Embed(title="------------------------------------------------------------------------------------------", description="------------------------------------------------------------------------------------------", color=0x0000ff)
+    embed.set_author(name="==============================================================")
+    embed.add_field(name="Hello! I'm Takashima-chan, an unofficial neko bot created by Clark#8056 and  AJ#2121! I have moderation and fun commands, some are still being added tho! I'm still being worked on by the developers, there are some errors here and there, but they're trying their best to finish me and add updates!", value="That's all for now, have a good day! nyah~", inline=False)
+    embed.add_field(name="-----------------------------------------------------------------------------------------", value="-----------------------------------------------------------------------------------------", inline=True)
+    embed.set_footer(text="=========================================================================")
+    await self.bot.say(embed=embed)
 
 @bot.command(name='8ball',
                 description="Answers a yes/no question.",

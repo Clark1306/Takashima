db8e2649daa028b0b65f7de66baa665d893e1beb
Clark1306 <45311467+Clark1306@users.noreply.github.com>
2018-12-24 17:33:08 +0800

    Update Takashima.py

--- Takashima.py
+++ Takashima.py
@@ -106,7 +106,7 @@ async def yn(context):
 @bot.command(pass_context=True)
 async def delete_channel(ctx, channel):
     server = ctx.message.server
-    await bot.delete_channel(channel, 'cool-channel')
+    await bot.delete_channel(channel)
 
 @bot.command(pass_context=True)
 async def say(ctx, *args):

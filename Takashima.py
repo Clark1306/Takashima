8975b74e86159ce8b6948af071932c613932dfee
Clark1306 <45311467+Clark1306@users.noreply.github.com>
2018-12-25 13:57:15 +0800

    Update Takashima.py

--- Takashima.py
+++ Takashima.py
@@ -78,9 +78,8 @@ async def about(ctx):
 
     embed.set_author(name="==============================================================")
     embed.add_field(name="Hello! I'm Takashima-chan, an unofficial neko bot created by Clark#8056 and  AJ#2121! I have moderation and fun commands, some are still being added tho! I'm still being worked on by the developers, there are some errors here and there, but they're trying their best to finish me and add updates!", value="That's all for now, have a good day! nyah~", inline=False)
-    embed.add_field(name="-----------------------------------------------------------------------------------------", value="-----------------------------------------------------------------------------------------", inline=True)
     embed.set_footer(text="=========================================================================")
-
+    
     await bot.send_message(channel, embed=embed)
 
 @bot.command(name='8ball',

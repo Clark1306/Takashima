72a9aba5d82332b01f7f6ae1c2bb53f11d7768cb
Clark1306 <45311467+Clark1306@users.noreply.github.com>
2018-12-27 20:22:43 +0800

    Update Takashima.py

--- Takashima.py
+++ Takashima.py
@@ -143,6 +143,16 @@ async def unban(ctx):
         await bot.say("Unban failed.")
         return
 
+@bot.command(pass_context = True)
+async def mute(ctx, user_id, userName: discord.User):
+    if ctx.message.author.server_permissions.administrator:
+        user = ctx.message.author
+        role = discord.utils.get(user.server.roles, name="Muted")
+        await bot.add_roles(user, role)
+     else:
+       embed=discord.Embed(title="Permission Denied.", description="You don't have permission to use this command.", color=0xff00f6)
+       await bot.say(embed=embed)    
+
 @bot.command(pass_context=True)
 async def clear(ctx, amount=999999999999999999999999999999999999999999999999):
     channel = ctx.message.channel

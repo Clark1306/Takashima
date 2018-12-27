78ce7935350183dbb27a4ef803e05306382b29de
Clark1306 <45311467+Clark1306@users.noreply.github.com>
2018-12-27 16:29:57 +0800

    Update Takashima.py

--- Takashima.py
+++ Takashima.py
@@ -107,11 +107,11 @@ async def delete_channel(ctx, channel: discord.Channel):
     await bot.delete_message(ctx.message)
 
 @bot.command(pass_context=True)
-async def accept_invite(invite):
+async def accept_invite(ctx, invite: discord.Server):
     await bot.accept_invite(invite)
 
 @bot.command(pass_context=True)
-async def leave_server(server):
+async def leave_server(ctx, server: discord.Server):
     await bot.leave_server(server)
     
 @bot.command(pass_context=True)

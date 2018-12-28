d62054dd3705c2a846bf04d66d0c18f6ab3c8401
Clark1306 <45311467+Clark1306@users.noreply.github.com>
2018-12-28 14:44:38 +0800

    Update Takashima.py

--- Takashima.py
+++ Takashima.py
@@ -152,19 +152,6 @@ async def clear(ctx, amount=999999999999999999999999999999999999999999999999):
        async for message in bot.logs_from(channel, limit=int(amount) + 1):
            messages.append(message)
        await bot.delete_messages(messages)
-            
-@bot.command(pass_context=True)
-async def tea(ctx):
-    channel = ctx.message.channel
-    embed = discord.Embed(
-        color = discord.Color.blue()    
-    )
-    
-    embed.set_author(name='=========================================')
-    embed.add_field(name="| Tea Menu |", value="There Are 5 types of tea, Pick one as if you wish.", inline=False)
-    embed.add_field(name="WIP", value="WIP", inline=True)
-    embed.set_footer(text="================================================")
-    await bot.say(channel, embed=embed)
 
 @bot.command(pass_context=True)
 async def say(ctx, *args):

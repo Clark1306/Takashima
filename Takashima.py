eda0a690b34db2a4d21f32a35c8ab85f2f1afe8f
Clark1306 <45311467+Clark1306@users.noreply.github.com>
2019-10-16 16:12:11 +0800

    Update Takashima.py

--- Takashima.py
+++ Takashima.py
@@ -115,10 +115,10 @@ async def mute(ctx, member: discord.Member):
      if ctx.message.author.server_permissions.administrator or ctx.message.author.id == '194151340090327041':
         role = discord.utils.get(member.server.roles, name='Muted')
         await bot.add_roles(member, role)
-        embed=discord.Embed(title="User Muted!", description="**{0}** was muted by **{1}**!".format(member, ctx.message.author), color=000dff)
+        embed=discord.Embed(title="User Muted!", description="**{0}** was muted by **{1}**!".format(member, ctx.message.author), color=0x00dff)
         await bot.say(embed=embed)
      else:
-        embed=discord.Embed(title="Permission Denied.", description="You don't have permission to use this command.", color=000dff)
+        embed=discord.Embed(title="Permission Denied.", description="You don't have permission to use this command.", color=0x00dff)
         await bot.say(embed=embed)
     
 @bot.command(pass_context=True)

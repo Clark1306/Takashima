f7c54c46a401061fc646a5aa71178af662a79071
Clark1306 <45311467+Clark1306@users.noreply.github.com>
2019-10-16 15:55:56 +0800

    Update Takashima.py

--- Takashima.py
+++ Takashima.py
@@ -112,7 +112,7 @@ async def delete_channel(ctx, channel: discord.Channel):
 
 @bot.command(pass_context = True)
 async def mute(ctx, member: discord.Member):
-     if ctx.message.author.server_permissions.administrator or ctx.message.author.id == '194151340090327041':
+     if ctx.message.author.server_permissions.administrator
         role = discord.utils.get(member.server.roles, name='Muted')
         await bot.add_roles(member, role)
         embed=discord.Embed(title="User Muted!", description="**{0}** was muted by **{1}**!".format(member, ctx.message.author), color=0xff00f6)

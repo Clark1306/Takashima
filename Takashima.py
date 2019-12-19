23d55573eb004076b639717b7b4a17e966645c30
Clark1306 <45311467+Clark1306@users.noreply.github.com>
2019-12-19 16:54:49 +0800

    Update Takashima.py

--- Takashima.py
+++ Takashima.py
@@ -245,13 +245,24 @@ async def unban(ctx):
 #    await ctx.send('{} just got slapped for {}'.format(slapped, reason))
 
 @bot.command(pass_context=True)
-async def clear(ctx, amount=9999999999999999999999999999999999999999999999999):
-     if ctx.message.author.server_permissions.administrator or ctx.message.author.id == '194151340090327041':
-       channel = ctx.message.channel
-       messages = []
-       async for message in bot.logs_from(channel, limit=int(amount) + 1):
-           messages.append(message)
-       await bot.delete_messages(messages)
+@bot.has_permissions(manage_messages=True)
+async def clear(self, ctx, amount: int):
+    channel = ctx.channel
+    await channel.purge(limit=amount+1)
+    
+    await bot.say(f"(amount) messages have been deleted")
+    await bot.delete_message(ctx.message)
+    
+@clear.error
+async def clear_error(self, ctx, error):
+    if isinstance(error, commands.CheckFailure):
+        await bot.say("You don't have permission to delete.")
+    elif isinstance(error, commands.MissingRequiredArgument):
+        await bot.say("There's no amount of numbers that i could delete.")
+    if isinstance(error, commands.BadArgument):
+        await bot.say("That's not a number.")
+        
+    raise error
 
 @bot.command(pass_context=True)
 async def say(ctx, *args):

0a8bec4e8e83621658a476d1d63069f7e13b1915
Clark1306 <45311467+Clark1306@users.noreply.github.com>
2019-12-19 19:16:39 +0800

    Update Takashima.py

--- Takashima.py
+++ Takashima.py
@@ -185,7 +185,7 @@ async def say(ctx, *args):
 
 
 @bot.command()
-#@commands.is_owner()
+@discord.ext.commands.is_owner()
 async def reload(ctx, cog):
     try:
         bot.unload_extension(f"cogs.{cog}")

71a7314b9c326c14b9240bc68d7586750894c132
Clark1306 <45311467+Clark1306@users.noreply.github.com>
2019-12-19 20:31:16 +0800

    Update Moderation.py

--- cogs/Moderation.py
+++ cogs/Moderation.py
@@ -2,8 +2,8 @@ import discord
 from discord.ext import commands
 
 class Mod:
-  def _init_(self, bot):
-    self.bot + bot
+  def __init__(self, bot):
+    self.bot = bot
 
 @commands.command()
 @commands.has_permissions(manage_messages=True)

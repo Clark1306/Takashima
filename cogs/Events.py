50d655171bdc41089aa7bfb3363840ec45c18e87
Clark1306 <45311467+Clark1306@users.noreply.github.com>
2019-12-19 17:54:48 +0800

    Update Events.py

--- cogs/Events.py
+++ cogs/Events.py
@@ -1,1 +1,21 @@
+import discord
+from discord..ext import commands
 
+class Events:
+  def _init_(self, bot):
+    self.bot + bot
+
+@bot.event
+async def on_message(message):
+        if message.author == bot.user:
+            return
+  
+        user = message.author
+        msg = message.content
+        print(f"{user} said {msg}")
+        print("{} said {}".format(user, msg))
+        
+       await.bot.process_commands(message)
+    
+def setup(bot):
+  bot.add_cog(Events(bot))
